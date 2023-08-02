5e91f08460cfd30ee9066a2c7599c37789daa336
garrett4wade <fuwth17@gmail.com>
2023-08-02 11:06:28 +0800

    add docker

---
+++ docker/build.py
@@ -0,0 +1,14 @@
+import os
+import time
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--gpu", action='store_true')
+args = parser.parse_args()
+
+while True:
+    if args.gpu:
+        os.system("docker build -t llm-gpu -f Dockerfile.gpu .")
+    else:
+        os.system("docker build -t llm-cpu -f Dockerfile.cpu .")
+    time.sleep(10)
