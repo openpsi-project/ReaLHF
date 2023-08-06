5fb44ec925ae699c9bdf949e0af5da92f89fbf16
garrett4wade <fuwth17@gmail.com>
2023-08-06 16:59:21 +0800

    add reward plotting scipt

---
+++ scripts/plot_reward.py
@@ -0,0 +1,19 @@
+import os
+import subprocess
+
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+experiment_name = "wps-rlhf"
+trial_name = "test20230806-5"
+
+reward_lines = subprocess.check_output(
+    [f"cat /home/aigc/llm/fw/logs/{experiment_name}_{trial_name}/master_worker | grep task_reward"],
+    shell=True).decode('ascii').split('\n')[:-1]
+rewards = np.array([float(line.split("'task_reward': ")[-1].split(',')[0]) for line in reward_lines])
+print(rewards)
+# rewards = np.convolve(rewards, np.ones(10) / 10, mode='same')
+
+plt.plot(rewards)
+plt.savefig("reward.png")
