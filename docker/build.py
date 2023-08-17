49d6942587c5cfa0ffbd5b5b66aaf4efb47ef0b4
garrett4wade <fuwth17@gmail.com>
2023-08-17 22:37:49 +0800

    update dockerfile

--- docker/build.py
+++ docker/build.py
@@ -13,8 +13,8 @@ if __name__ == "__main__":
     assert args.gpu != args.cpu
     diip = "10.122.2.14" if 'ctrl' not in socket.gethostname() else "10.210.14.10"
     identi = "cpu" if args.cpu else "gpu"
-    for _ in range(1000):
+    for _ in range(100):
         os.system(f"docker build -t {diip}:5000/llm/llm-{identi} "
                   f"-f docker/Dockerfile.{identi} /local/fw/packages/docker")
-        time.sleep(5)
+        time.sleep(1)
     os.system(f"docker push {diip}:5000/llm/llm-{identi}")
