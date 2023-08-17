50589b1786cef8af4257cc311eff09f5e8c7ef00
garrett4wade <fuwth17@gmail.com>
2023-08-17 23:03:21 +0800

    .

--- docker/build.py
+++ docker/build.py
@@ -14,7 +14,12 @@ if __name__ == "__main__":
     diip = "10.122.2.14" if 'ctrl' not in socket.gethostname() else "10.210.14.10"
     identi = "cpu" if args.cpu else "gpu"
     for _ in range(100):
-        os.system(f"docker build -t {diip}:5000/llm/llm-{identi} "
-                  f"-f docker/Dockerfile.{identi} /local/fw/packages/docker")
+        cmd = (f"docker build -t {diip}:5000/llm/llm-{identi} "
+               f"-f docker/Dockerfile.{identi}")
+        if args.gpu and 'ctrl' in socket.gethostname():
+            cmd += " --build-arg base_image=nvcr.io/nvidia/pytorch:22.04-py3"
+        cmd += " /local/fw/packages/docker"
+        print(cmd)
+        os.system(cmd)
         time.sleep(1)
     os.system(f"docker push {diip}:5000/llm/llm-{identi}")
