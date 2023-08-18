1b0b28d4d20b1473b5367537c9b0938255fc77a1
garrett4wade <fuwth17@gmail.com>
2023-08-18 16:32:12 +0800

    .

--- docker/build.py
+++ docker/build.py
@@ -6,6 +6,7 @@ import socket
 parser = argparse.ArgumentParser()
 parser.add_argument("--gpu", action='store_true')
 parser.add_argument("--cpu", action="store_true")
+parser.add_argument("--retry", type=int, default=100)
 args = parser.parse_args()
 
 if __name__ == "__main__":
@@ -13,7 +14,7 @@ if __name__ == "__main__":
     assert args.gpu != args.cpu
     diip = "10.122.2.14" if 'ctrl' not in socket.gethostname() else "10.210.14.10"
     identi = "cpu" if args.cpu else "gpu"
-    for _ in range(100):
+    for _ in range(args.retry):
         cmd = (f"docker build -t {diip}:5000/llm/llm-{identi} "
                f"-f docker/Dockerfile.{identi}")
         if args.gpu and 'ctrl' in socket.gethostname():
