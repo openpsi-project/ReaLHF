6c32496c186521b7d8097c2519b4a58d81be86a2
fw <fuwth17@gmail.com>
2023-12-04 15:03:36 +0800

    fix init from scratch in flash mqat

--- impl/model/nn/flash_mqat/flash_mqat_interface.py
+++ impl/model/nn/flash_mqat/flash_mqat_interface.py
@@ -10,8 +10,12 @@ import torch.utils.checkpoint
 import transformers
 
 from impl.model.nn.flash_mqat.flash_generate import generate, GenerationConfig
-from impl.model.nn.flash_mqat.flash_mqat_base import (FlashMQATBase, FlashMQATBlock, FlashMQATConfig,
-                                                      FlashMQATForCausalLM)
+from impl.model.nn.flash_mqat.flash_mqat_base import (
+    FlashMQATBase,
+    FlashMQATBlock,
+    FlashMQATConfig,
+    FlashMQATForCausalLM,
+)
 from impl.model.utils.data import DuckGenerationOutput, DuckModelOutput, PipeCacheData, PipeTransferData
 from impl.model.utils.save_load import load_from_disk
 import api.huggingface
@@ -60,12 +64,14 @@ class HuggingfaceLikeFlashMQATForCausalLM(nn.Module):
             build_packed = True
         if packed_input_ids is not None:
             x = PipeTransferData(cu_seqlens=cu_seqlens, max_seqlen=max_seqlen)
-            ys = [PipeCacheData(input_ids=packed_input_ids)
-                  ] + [PipeCacheData() for _ in range(self.config.n_layers + 1)]
+            ys = [PipeCacheData(input_ids=packed_input_ids)] + [
+                PipeCacheData() for _ in range(self.config.n_layers + 1)
+            ]
         else:
             x = PipeTransferData()
-            ys = [PipeCacheData(input_ids=input_ids)
-                  ] + [PipeCacheData() for _ in range(self.config.n_layers + 1)]
+            ys = [PipeCacheData(input_ids=input_ids)] + [
+                PipeCacheData() for _ in range(self.config.n_layers + 1)
+            ]
         logits = self.net(x, ys).pp_output
         if build_packed:
             logits = pad_input(logits, indices, batch_size, seqlen)
@@ -97,15 +103,17 @@ class HuggingfaceLikeFlashMQATForCausalLM(nn.Module):
     def from_pretrained(
         cls,
         model_path: str,
+        init_from_scratch: bool = False,
         dtype: Optional[torch.dtype] = None,
         device: Optional[Union[str, torch.device]] = None,
     ):
         with open(os.path.join(model_path, "config.json"), "r") as f:
             config = FlashMQATConfig(**json.load(f))
-        state_dict = load_from_disk(model_path)
         net = FlashMQATForCausalLM(config, dtype, device)
         model = cls(net)
-        model.load_state_dict(state_dict)
+        if not init_from_scratch:
+            state_dict = load_from_disk(model_path)
+            model.load_state_dict(state_dict)
         return model
 
 
@@ -116,19 +124,21 @@ def make_flash_mqat_clm_hf(
     dtype: Optional[torch.dtype] = None,
     from_type: str = "starcoder",
     tokenizer_path: Optional[str] = None,
+    init_from_scratch: bool = False,
 ) -> api.model.Model:
     if from_type == "self":
-        module = HuggingfaceLikeFlashMQATForCausalLM.from_pretrained(model_path=model_path,
-                                                                     dtype=dtype,
-                                                                     device=device)
+        module = HuggingfaceLikeFlashMQATForCausalLM.from_pretrained(
+            model_path=model_path, init_from_scratch=init_from_scratch, dtype=dtype, device=device
+        )
         if tokenizer_path is None:
             raise ValueError("tokenizer_path must be provided when from_type is 'self'.")
         tokenizer = api.huggingface.load_hf_tokenizer(tokenizer_path)
     else:
         module = HuggingfaceLikeFlashMQATForCausalLM(
-            getattr(FlashMQATForCausalLM, f"from_{from_type}")(model_path=model_path,
-                                                               dtype=dtype,
-                                                               device=device))
+            getattr(FlashMQATForCausalLM, f"from_{from_type}")(
+                model_path=model_path, dtype=dtype, device=device, init_from_scratch=init_from_scratch
+            ),
+        )
         tokenizer = api.huggingface.load_hf_tokenizer(model_path)
     return api.model.Model(name, module, tokenizer, device)
 
@@ -137,15 +147,12 @@ api.model.register_model("flash_mqat_clm_hf", make_flash_mqat_clm_hf)
 
 
 class DeepSpeedChatLikeFlashMQATCriticModel(nn.Module):
-
     def __init__(self, net: FlashMQATBase, output_scaling: float = 1.0, output_bias: float = 0.0):
         super().__init__()
         self.net = net
-        self.head = nn.Linear(net.config.hidden_dim,
-                              1,
-                              bias=False,
-                              dtype=self.net.dtype,
-                              device=self.net.device)
+        self.head = nn.Linear(
+            net.config.hidden_dim, 1, bias=False, dtype=self.net.dtype, device=self.net.device
+        )
         self.output_scaling = output_scaling
         self.output_bias = output_bias
 
@@ -174,8 +181,9 @@ class DeepSpeedChatLikeFlashMQATCriticModel(nn.Module):
             batch_size, seqlen = input_ids
         if packed_input_ids is not None:
             x = PipeTransferData(cu_seqlens=cu_seqlens, max_seqlen=max_seqlen)
-            ys = [PipeCacheData(input_ids=packed_input_ids)
-                  ] + [PipeCacheData() for _ in range(self.config.n_layers)]
+            ys = [PipeCacheData(input_ids=packed_input_ids)] + [
+                PipeCacheData() for _ in range(self.config.n_layers)
+            ]
         else:
             x = PipeTransferData()
             ys = [PipeCacheData(input_ids=input_ids)] + [PipeCacheData() for _ in range(self.config.n_layers)]
@@ -189,13 +197,16 @@ class DeepSpeedChatLikeFlashMQATCriticModel(nn.Module):
         cls,
         from_model: Optional[HuggingfaceLikeFlashMQATForCausalLM] = None,
         model_path: Optional[str] = None,
+        init_from_scratch: bool = False,
         dtype: Optional[torch.dtype] = None,
         device: Optional[Union[str, torch.device]] = None,
         output_scaling: float = 1.0,
         output_bias: float = 0.0,
     ):
         if from_model is None:
-            from_model = HuggingfaceLikeFlashMQATForCausalLM.from_pretrained(model_path, dtype, device)
+            from_model = HuggingfaceLikeFlashMQATForCausalLM.from_pretrained(
+                model_path, init_from_scratch=init_from_scratch, dtype=dtype, device=device
+            )
         model = cls(from_model.net.transformer, output_bias=output_bias, output_scaling=output_scaling)
         return model
 
@@ -203,6 +214,7 @@ class DeepSpeedChatLikeFlashMQATCriticModel(nn.Module):
     def from_pretrained(
         cls,
         model_path: str,
+        init_from_scratch: bool = False,
         dtype: Optional[torch.dtype] = None,
         device: Optional[Union[str, torch.device]] = None,
         output_scaling: float = 1.0,
@@ -210,10 +222,11 @@ class DeepSpeedChatLikeFlashMQATCriticModel(nn.Module):
     ):
         with open(os.path.join(model_path, "config.json"), "r") as f:
             config = FlashMQATConfig(**json.load(f))
-        state_dict = load_from_disk(model_path)
         net = FlashMQATBase(config, dtype, device)
         model = cls(net, output_bias=output_bias, output_scaling=output_scaling)
-        model.load_state_dict(state_dict)
+        if not init_from_scratch:
+            state_dict = load_from_disk(model_path)
+            model.load_state_dict(state_dict)
         return model
 
 
@@ -221,6 +234,7 @@ def make_flash_mqat_critic(
     name: str,
     device: torch.device,
     model_path: str,
+    init_from_scratch: bool = False,
     dtype: Optional[torch.dtype] = None,
     from_type: str = "sft",
     tokenizer_path: Optional[str] = None,
@@ -235,6 +249,7 @@ def make_flash_mqat_critic(
             model_path=model_path,
             dtype=dtype,
             device=device,
+            init_from_scratch=init_from_scratch,
             output_scaling=output_scaling,
             output_bias=output_bias,
         )
@@ -243,6 +258,7 @@ def make_flash_mqat_critic(
             model_path=model_path,
             dtype=dtype,
             device=device,
+            init_from_scratch=init_from_scratch,
             output_scaling=output_scaling,
             output_bias=output_bias,
         )
@@ -253,13 +269,14 @@ def make_flash_mqat_critic(
             model_path=model_path,
             dtype=dtype,
             from_type=from_type,
+            init_from_scratch=init_from_scratch,
             tokenizer_path=tokenizer_path,
         )
-        module = DeepSpeedChatLikeFlashMQATCriticModel(from_model.module.net.transformer,
-                                                       output_bias=output_bias,
-                                                       output_scaling=output_scaling)
-        if v_head_path is not None:
-            module.head.load_state_dict(torch.load(v_head_path))
+        module = DeepSpeedChatLikeFlashMQATCriticModel(
+            from_model.module.net.transformer, output_bias=output_bias, output_scaling=output_scaling
+        )
+    if v_head_path is not None and not init_from_scratch:
+        module.head.load_state_dict(torch.load(v_head_path))
     tokenizer = api.huggingface.load_hf_tokenizer(tokenizer_path)
     return api.model.Model(name, module, tokenizer, device)
 
