f64215e4f19f85398e62fe7e236a5746bd322a0e
fw <fuwth17@gmail.com>
2023-12-24 20:29:42 +0800

    .

--- base/cluster.py
+++ base/cluster.py
@@ -92,7 +92,7 @@ class QHClusterSpec(ClusterSpec):
 
     @property
     def default_mount(self) -> str:
-        return "/lustre:/lustre,/dev/infiniband:/dev/infiniband,/sys/class/infiniband_verbs:/sys/class/infiniband_verbs,/home/fw/tools/DeepSpeed-master:/DeepSpeed"
+        return "/lustre:/lustre,/dev/infiniband:/dev/infiniband,/sys/class/infiniband_verbs:/sys/class/infiniband_verbs"
 
 
 class YLClusterSpec(ClusterSpec):
