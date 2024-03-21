3c45f96b15bb7bd9fab47e2a43c3c3c5396a9c1b
fw <fuwth17@gmail.com>
2024-03-21 09:45:03 +0800

    fix reparallelize

--- test_reparallelize.py
+++ test_reparallelize.py
@@ -8,7 +8,7 @@ import os
 import time
 
 import torch.distributed
-from api.config.config_system import ModelShardID
+from api.config.config_system import ModelShardID, ModelName
 from base.topology import PipeModelDataParallelTopology
 import itertools
 
@@ -59,47 +59,65 @@ def setup_comm_groups(from_topo: PipeModelDataParallelTopology, to_topo: PipeMod
     comm_groups = {}
     comm_src = {}
     comm_dst_ranks = {}
-    for src_pp_rank, dst_pp_rank in itertools.product(
-        range(from_topo.get_dim("pipe")), range(to_topo.get_dim("pipe"))
-    ):
+    for pp_i, pp_j in itertools.product(range(from_topo.get_dim("pipe")), range(to_topo.get_dim("pipe"))):
         # create tensor reshard groups
         src_mp_size = from_topo.get_dim("model")
         dst_mp_size = to_topo.get_dim("model")
 
         for mp_j in range(dst_mp_size):
-            _all_dst_ranks = to_topo.filter_match(pipe=dst_pp_rank, model=mp_j)
+            _all_dst_ranks = to_topo.filter_match(pipe=pp_j, model=mp_j)
             if src_mp_size > dst_mp_size:
                 factor = src_mp_size // dst_mp_size
                 mp_is = list(range(factor * mp_j, factor * (mp_j + 1)))
-                _all_src_ranks = [
-                    from_topo.filter_match(model=mp_i, pipe=src_pp_rank) for mp_i in mp_is
-                ]
+                _all_src_ranks = [from_topo.filter_match(model=mp_i, pipe=pp_i) for mp_i in mp_is]
             else:
                 factor = dst_mp_size // src_mp_size
-                _all_src_ranks = [from_topo.filter_match(model=mp_j // factor, pipe=src_pp_rank)]
+                _all_src_ranks = [from_topo.filter_match(model=mp_j // factor, pipe=pp_i)]
             for receiver_portion_id, _src_ranks in enumerate(_all_src_ranks):
                 # All GPUs in _src_ranks have the data required by (pp_j, mp_j)
                 # We split _dst_ranks evenly among the GPUs in _src_ranks, such that they can broadcast in parallel.
                 subgroup_size = (len(_all_dst_ranks) + len(_src_ranks) - 1) // len(_src_ranks)
-                for subgroup_id, _src_rank in enumerate(_src_ranks):
-                    _dst_ranks = _all_dst_ranks[
-                        subgroup_id * subgroup_size : (subgroup_id + 1) * subgroup_size
+                _grouped_dst_ranks = [
+                    [
+                        _x + 8 - to_topo.world_size()
+                        for _x in _all_dst_ranks[
+                            subgroup_id * subgroup_size : (subgroup_id + 1) * subgroup_size
+                        ]
                     ]
-                    _dst_ranks = [_x + 8 - to_topo.world_size() for _x in _dst_ranks]
+                    for subgroup_id in range(len(_src_ranks))
+                ]
+                # prioritize the _src_rank that is also in _dst_ranks
+                _src_rank_orders = []
+                for _dst_ranks in _grouped_dst_ranks:
+                    appended = False
+                    for j, _src_rank in enumerate(_src_ranks):
+                        if j in _src_rank_orders:
+                            continue
+                        if _src_rank in _dst_ranks:
+                            _src_rank_orders.append(j)
+                            appended = True
+                            break
+                    if not appended:
+                        _src_rank_orders.append(
+                            next(j for j in range(len(_src_ranks)) if j not in _src_rank_orders)
+                        )
+                assert set(_src_rank_orders) == set(range(len(_src_ranks)))
+                for _dst_ranks, _src_rank_id in zip(_grouped_dst_ranks, _src_rank_orders):
+                    _src_rank = _src_ranks[_src_rank_id]
 
                     dp_i, mp_i = (
                         from_topo.get_coord(_src_rank).data,
                         from_topo.get_coord(_src_rank).model,
                     )
-                    comm_dst_ranks[(dp_i, mp_i, src_pp_rank, mp_j, dst_pp_rank)] = _dst_ranks
+                    comm_dst_ranks[(dp_i, mp_i, pp_i, mp_j, pp_j)] = _dst_ranks
                     if _src_rank not in _dst_ranks:
                         _dst_ranks = [_src_rank] + _dst_ranks
                     assert len(set(_dst_ranks)) == len(_dst_ranks)
                     if len(_dst_ranks) > 1:
-                        comm_groups[(dp_i, mp_i, src_pp_rank, mp_j, dst_pp_rank)] = dist.new_group(_dst_ranks)
+                        comm_groups[(dp_i, mp_i, pp_i, mp_j, pp_j)] = dist.new_group(_dst_ranks)
                     else:
-                        comm_groups[(dp_i, mp_i, src_pp_rank, mp_j, dst_pp_rank)] = None
-                    comm_src[(dp_i, mp_i, src_pp_rank, mp_j, dst_pp_rank)] = _src_rank
+                        comm_groups[(dp_i, mp_i, pp_i, mp_j, pp_j)] = None
+                    comm_src[(dp_i, mp_i, pp_i, mp_j, pp_j)] = _src_rank
 
     return comm_groups, comm_src, comm_dst_ranks
 
@@ -114,9 +132,11 @@ def worker(idx: int, from_topo: PipeModelDataParallelTopology, to_topo: PipeMode
     global_state_dict = create_global_param(param_size, n_layers)
 
     if idx < from_topo.world_size():
-        from_shard_id = ModelShardID.from_parallelism_rank("default", from_topo, idx)
-    if idx >= 8-to_topo.world_size():
-        to_shard_id = ModelShardID.from_parallelism_rank("default", to_topo, idx - (8-to_topo.world_size()))
+        from_shard_id = ModelShardID.from_parallelism_rank(ModelName("default", 0), from_topo, idx)
+    if idx >= 8 - to_topo.world_size():
+        to_shard_id = ModelShardID.from_parallelism_rank(
+            ModelName("default", 0), to_topo, idx - (8 - to_topo.world_size())
+        )
 
     dst_mp_size = to_topo.get_dim("model")
     src_mp_size = from_topo.get_dim("model")
@@ -127,7 +147,9 @@ def worker(idx: int, from_topo: PipeModelDataParallelTopology, to_topo: PipeMode
 
         _param_size_per_mp = param_size // from_topo.get_dim("model")
         state_dict = {
-            i: x[from_shard_id.mp_rank * _param_size_per_mp : (from_shard_id.mp_rank + 1) * _param_size_per_mp]
+            i: x[
+                from_shard_id.mp_rank * _param_size_per_mp : (from_shard_id.mp_rank + 1) * _param_size_per_mp
+            ]
             for i, x in global_state_dict.items()
             if i in range(from_layer_idx_start, from_layer_idx_end)
         }
@@ -171,8 +193,7 @@ def worker(idx: int, from_topo: PipeModelDataParallelTopology, to_topo: PipeMode
     # derive a global NCCL communication plan
     for (pp_i, pp_j), layer_indices in repart_strat.items():
         sub_sd = {
-            i: (get_param_shape(i, from_topo.get_dim("model")), get_param_dtype(i))
-            for i in layer_indices
+            i: (get_param_shape(i, from_topo.get_dim("model")), get_param_dtype(i)) for i in layer_indices
         }
         for k, (shape, dtype) in sub_sd.items():
             for mp_i in range(src_mp_size):
@@ -245,6 +266,7 @@ def worker(idx: int, from_topo: PipeModelDataParallelTopology, to_topo: PipeMode
 
     # input()
 
+    comm_volume = 0
     new_state_dict = {}
     for step in comm_plan:
         portion_size = min(param_size // dst_mp_size, param_size // src_mp_size)
@@ -260,6 +282,7 @@ def worker(idx: int, from_topo: PipeModelDataParallelTopology, to_topo: PipeMode
                 ]
             else:
                 buf = torch.zeros(step.param_shape, dtype=step.param_dtype, device="cuda")
+                comm_volume += buf.numel()
                 torch.distributed.broadcast(buf, src=step.src, group=step.group)
 
             new_state_dict[step.param_key][
@@ -270,13 +293,14 @@ def worker(idx: int, from_topo: PipeModelDataParallelTopology, to_topo: PipeMode
                 buf = state_dict[step.param_key][
                     step.sender_mp_portion_id * portion_size : (step.sender_mp_portion_id + 1) * portion_size
                 ]
+                # comm_volume += buf.numel() * len(step.dst_ranks)
                 torch.distributed.broadcast(buf, src=step.rank, group=step.group)
             if step.remove:
                 state_dict.pop(step.param_key)
 
     assert len(state_dict) == 0, (idx, state_dict.keys(), new_state_dict.keys())
 
-    if idx >= 8-to_topo.world_size():
+    if idx >= 8 - to_topo.world_size():
         assert len(new_state_dict) == n_layers // to_topo.get_dim("pipe"), len(new_state_dict)
         to_layer_idx_start = n_layers // to_topo.get_dim("pipe") * to_shard_id.pp_rank
         to_layer_idx_end = n_layers // to_topo.get_dim("pipe") * (to_shard_id.pp_rank + 1)
@@ -291,6 +315,59 @@ def worker(idx: int, from_topo: PipeModelDataParallelTopology, to_topo: PipeMode
             ]
             assert torch.allclose(new_state_dict[i], a), (new_state_dict[i], a, i)
 
+    layer_indices_before = {}
+    param_portion_before = {}
+    for i in range(8):
+        if i < from_topo.world_size():
+            coord = from_topo.get_coord(i)
+            layer_indices_before[i] = list(
+                range(
+                    coord.pipe * n_layers // from_topo.get_dim("pipe"),
+                    (coord.pipe + 1) * n_layers // from_topo.get_dim("pipe"),
+                )
+            )
+            param_portion_before[i] = (
+                coord.model * param_size // from_topo.get_dim("model"),
+                (coord.model + 1) * param_size // from_topo.get_dim("model"),
+            )
+        else:
+            layer_indices_before[i] = []
+            param_portion_before[i] = (0, 0)
+
+    layer_indices_after = {}
+    param_portion_after = {}
+    for i in range(8):
+        if i >= 8 - to_topo.world_size():
+            coord = to_topo.get_coord(i - (8 - to_topo.world_size()))
+            layer_indices_after[i] = list(
+                range(
+                    coord.pipe * n_layers // to_topo.get_dim("pipe"),
+                    (coord.pipe + 1) * n_layers // to_topo.get_dim("pipe"),
+                )
+            )
+            param_portion_after[i] = (
+                coord.model * param_size // to_topo.get_dim("model"),
+                (coord.model + 1) * param_size // to_topo.get_dim("model"),
+            )
+        else:
+            layer_indices_after[i] = []
+            param_portion_after[i] = (0, 0)
+
+    est_comm_volume = 0
+    for i in range(8):
+        for layer_idx in layer_indices_after[i]:
+            if layer_idx in layer_indices_before[i]:
+                est_comm_volume += len(
+                    set(range(param_portion_after[i][0], param_portion_after[i][1])).difference(
+                        range(param_portion_before[i][0], param_portion_before[i][1])
+                    )
+                )
+            else:
+                est_comm_volume += param_portion_after[i][1] - param_portion_after[i][0]
+
+    comm_volume = torch.tensor(comm_volume, dtype=torch.int32, device="cuda")
+    torch.distributed.all_reduce(comm_volume)
+    assert comm_volume == est_comm_volume, (comm_volume, est_comm_volume, from_topo, to_topo)
     torch.distributed.barrier()
     if idx == 0:
         print("success!")
@@ -308,8 +385,7 @@ def decompose_to_three_factors(n: int):
 
 
 if __name__ == "__main__":
-    decompositions = decompose_to_three_factors(6)
-    for x1, x2 in itertools.product(decompositions, decompositions):
+    for x1, x2 in itertools.product(decompose_to_three_factors(8), decompose_to_three_factors(8)):
         if not (x1[1] % x2[1] == 0 or x2[1] % x1[1] == 0):
             continue
         print(f"testing from {x1} to {x2}")
