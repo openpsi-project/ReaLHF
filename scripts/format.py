f4b16d5f1d4cc6b297d3b9184e0d86deab37c0c1
fw <fuwth17@gmail.com>
2024-04-23 02:12:36 +0000

    .

---
+++ scripts/format.py
@@ -0,0 +1,11 @@
+import os
+import multiprocessing
+
+
+def format_file(file_path):
+    os.system(f"yapf -rip {file_path} && isort {file_path}")
+
+
+if __name__ == "__main__":
+    pool = multiprocessing.Pool()
+    pool.map(format_file, ["api", "base", "impl", "scheduler", "apps", "experiments", "system"])
---
+++ scripts/test_srun.sh
@@ -0,0 +1,5 @@
+srun --container-image=llm/llm-gpu \
+    --nodelist=QH-com04 \
+    --container-mounts=/lustre:/lustre -N1 \
+    --pty -c99 --mem=900G --gpus=8 --container-mount-home bash
+# --exclude=QH-com[01-14],QH-com29,QH-com35 \
