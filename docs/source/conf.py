85f2bce202d46b2d6cb9f57b007300d6ae73642d
fw <fuwth17@gmail.com>
2024-06-11 02:42:18 +0000

    format

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,27 +6,25 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'ReaL-LLM'
-copyright = '2024, Wei Fu & Zhiyu Mei'
-author = 'Wei Fu & Zhiyu Mei'
-release = '0.1.0'
+project = "ReaL-LLM"
+copyright = "2024, Wei Fu & Zhiyu Mei"
+author = "Wei Fu & Zhiyu Mei"
+release = "0.1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_nefertiti'
-html_static_path = ['_static']
+html_theme = "sphinx_nefertiti"
+html_static_path = ["_static"]
 
 extensions = [
-    'sphinx.ext.duration',
+    "sphinx.ext.duration",
 ]
