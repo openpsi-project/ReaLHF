27b4cd5c08c7dcf97394639a13d63885a1f45011
fw <fuwth17@gmail.com>
2024-07-15 04:56:31 +0000

    .

--- realhf/api/from_hf/__init__.py
+++ realhf/api/from_hf/__init__.py
@@ -3,10 +3,7 @@ import re
 
 from realhf.base.importing import import_module
 
-# HACK: StarCoder seems to have a bug with transformers v0.39.1:
 # load_state_dict does not work on this model.
 # The weights are not changed after loading.
 # Skip this model temporarily.
-import_module(
-    os.path.dirname(__file__), re.compile(r"^(?!.*(__init__|starcoder)).*\.py$")
-)
+import_module(os.path.dirname(__file__), re.compile(r"^(?!.*__init__).*\.py$"))
