bfeaf1fc565b78ab349684a17e61b86bd5d14d02
Wei Fu <36355462+garrett4wade@users.noreply.github.com>
2024-07-17 10:20:49 +0800

    [BUGFIX] Fix dockerfile and some known bugs. (#32)
    * .
    
    * .
    
    * remove data key remap
    
    * .
    
    * .
    
    * sft run
    
    * .
    
    * format
    
    * minor revert
    
    * refactor finish
    
    * .
    
    * minor fix
    
    * add gen script
    
    * fix
    
    * fix dataflow graph visualization save path
    
    * .
    
    * add SequenceSample class and tests
    
    * simple renaming
    
    * sft run
    
    * format
    
    * rw run
    
    * dpo run
    
    * gen run
    
    * ppo run
    
    * format
    
    * fix all
    
    * minor fix
    
    * minor fix
    
    * minor change
    
    * refactor and fix tests
    
    * fix
    
    * .
    
    * .
    
    * .
    
    * shrink testing model size
    
    * .
    
    * update pytest mark
    
    * .
    
    * .
    
    * fix
    
    * .
    
    * .
    
    * [MAJOR] Refactor the data structure to support multiple sequences in a single piece of data. (#27)
    
    * .
    
    * .
    
    * remove data key remap
    
    * .
    
    * .
    
    * sft run
    
    * .
    
    * format
    
    * minor revert
    
    * refactor finish
    
    * .
    
    * minor fix
    
    * add gen script
    
    * fix
    
    * fix dataflow graph visualization save path
    
    * .
    
    * add SequenceSample class and tests
    
    * simple renaming
    
    * sft run
    
    * format
    
    * rw run
    
    * dpo run
    
    * gen run
    
    * ppo run
    
    * format
    
    * fix all
    
    * minor fix
    
    * minor fix
    
    * minor change
    
    * fix
    
    * .
    
    * add reinforce algo
    
    * ensure learning
    
    * mv reinforce impl to ./examples
    
    * fix typo
    
    * add key remap back
    
    * .
    
    * .
    
    * add the grpo algorithm
    
    * fix
    
    * fix param realloc test
    
    * .
    
    * refine interval op kernel
    
    * .
    
    * support passing metadata
    
    * .
    
    * fix
    
    * .
    
    * .
    
    * .
    
    * .

--- tests/cpp_extensions/test_interval_ops.py
+++ tests/cpp_extensions/test_interval_ops.py
@@ -21,10 +21,13 @@ def make_intervals(maxsize, n_intervals):
     s = maxsize // n_intervals
     intervals = []
     interval_size = 0
+    max_interval_size = 0
     for i in range(n_intervals):
         intervals.append((i * s, i * s + s // 2))
         interval_size += s // 2
-    return np.array(intervals, dtype=np.int64), interval_size
+        max_interval_size = max(max_interval_size, s // 2)
+    np.random.shuffle(intervals)
+    return np.array(intervals, dtype=np.int64), interval_size, max_interval_size
 
 
 def maybe_synchronize_cuda():
@@ -32,24 +35,40 @@ def maybe_synchronize_cuda():
         torch.cuda.synchronize()
 
 
-@pytest.mark.parametrize("device", [torch.device("cuda"), torch.device("cpu")])
-@pytest.mark.parametrize("n_intervals", list(reversed([1, 100, 10000, 100000])))
+@pytest.mark.skipif(not torch.cuda.is_available(), reason="This test requires a GPU.")
+@pytest.mark.parametrize(
+    "n_intervals", list(reversed([1, 100, 500, 1000, 2000, 4000, 10000, 100000]))
+)
 @pytest.mark.parametrize("dtype", [torch.bfloat16, torch.float32, torch.float16])
-def test_get(n_intervals: int, dtype: torch.dtype, device: torch.device):
-    if device == torch.device("cuda") and not torch.cuda.is_available():
-        return
+def test_get(n_intervals: int, dtype: torch.dtype):
+    device = torch.device("cuda")
 
     input_tensor = torch.randn(int(1e8), device=device, dtype=dtype)
-    intervals, _ = make_intervals(input_tensor.size(0), n_intervals)
+    intervals, output_size, max_interval_size = make_intervals(
+        input_tensor.size(0), n_intervals
+    )
+    intervals_cuda = torch.tensor(intervals, dtype=torch.long, device="cuda")
 
     # warmup
-    slice_intervals(input_tensor, intervals)
+    slice_intervals(
+        input_tensor,
+        intervals,
+        intervals_cuda=intervals_cuda,
+        output_size=output_size,
+        max_interval_size=max_interval_size,
+    )
     _slice_intervals_py(input_tensor, intervals)
 
     maybe_synchronize_cuda()
     tik = time.perf_counter()
     for _ in range(10):
-        output_tensor = slice_intervals(input_tensor, intervals)
+        output_tensor = slice_intervals(
+            input_tensor,
+            intervals,
+            intervals_cuda=intervals_cuda,
+            output_size=output_size,
+            max_interval_size=max_interval_size,
+        )
     maybe_synchronize_cuda()
     t1 = time.perf_counter() - tik
 
@@ -60,12 +79,14 @@ def test_get(n_intervals: int, dtype: torch.dtype, device: torch.device):
     maybe_synchronize_cuda()
     t2 = time.perf_counter() - tik
     assert torch.allclose(output_tensor, o2)
-    print(f"Success! C++ ext time: {t1:.4f}, PyTorch time: {t2:.4f}")
+    print(
+        f"slice_interval, Success! #intervals: {n_intervals} C++ ext time: {t1:.4f}, PyTorch time: {t2:.4f}"
+    )
 
 
 @pytest.mark.skipif(not torch.cuda.is_available(), reason="This test requires a GPU.")
 @pytest.mark.parametrize(
-    "n_intervals", list(reversed([1, 10, 100, 1000, 2048, 4096, 10000, 100000]))
+    "n_intervals", list(reversed([1, 10, 100, 500, 1000, 1000, 10000, 100000]))
 )
 @pytest.mark.parametrize("dtype", [torch.bfloat16, torch.float16, torch.float32])
 @pytest.mark.gpu
@@ -74,12 +95,19 @@ def test_set(n_intervals: int, dtype: torch.dtype):
     # We don't need to test it with CPU tensors.
 
     x = torch.randn(int(1e8), device="cuda", dtype=dtype)
-    intervals, interval_size = make_intervals(x.size(0), n_intervals)
+    intervals, interval_size, max_interval_size = make_intervals(x.size(0), n_intervals)
+    intervals_cuda = torch.tensor(intervals, dtype=torch.long, device="cuda")
     src = torch.randn(interval_size, device="cuda", dtype=dtype)
 
     # warmup
     input_tensor1 = x.clone()
-    set_intervals(src, input_tensor1, intervals)
+    set_intervals(
+        src,
+        input_tensor1,
+        intervals,
+        intervals_cuda=intervals_cuda,
+        max_interval_size=max_interval_size,
+    )
     input_tensor2 = x.clone()
     _set_intervals_py(src, input_tensor2, intervals)
 
@@ -87,7 +115,13 @@ def test_set(n_intervals: int, dtype: torch.dtype):
     maybe_synchronize_cuda()
     tik = time.perf_counter()
     for _ in range(10):
-        set_intervals(src, input_tensor1, intervals)
+        set_intervals(
+            src,
+            input_tensor1,
+            intervals,
+            intervals_cuda=intervals_cuda,
+            max_interval_size=max_interval_size,
+        )
     maybe_synchronize_cuda()
     t1 = time.perf_counter() - tik
 
@@ -100,4 +134,6 @@ def test_set(n_intervals: int, dtype: torch.dtype):
     t2 = time.perf_counter() - tik
 
     assert torch.allclose(input_tensor1, input_tensor2)
-    print(f"Success! C++ ext time: {t1:.4f}, PyTorch time: {t2:.4f}")
+    print(
+        f"set_interval, Success! #intervals: {n_intervals}, C++ ext time: {t1:.4f}, PyTorch time: {t2:.4f}"
+    )
